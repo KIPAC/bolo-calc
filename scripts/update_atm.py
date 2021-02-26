050829098cd83b548745d841ae18a9839bbd487c
Eric Charles <badass@stanford.edu>
2021-02-26 08:53:15 -0800

    Updated update_atm.py

--- scripts/update_atm.py
+++ scripts/update_atm.py
@@ -1,7 +1,8 @@
-import shutil as su
+#!/usr/bin/env python
+
 import urllib.request as ul
-import sys as sy
-import time as tm
+import sys
+import time
 import os
 
 # Progress monitor
@@ -11,9 +12,9 @@ def reporthook(count, block_size, total_size):
     # Download speed
     global start_time
     if count == 0:
-        start_time = tm.time()
+        start_time = time.time()
         return
-    duration = tm.time() - start_time
+    duration = time.time() - start_time
     progress_size = int(count * block_size)
     if duration == 0:
         msg = (
@@ -24,24 +25,29 @@ def reporthook(count, block_size, total_size):
         msg = (
             "\rDownloading atmosphere data: %d%%, %d MB, %d kB/s" %
             (percent, progress_size / (1024 * 1024), speed))
-    sy.stdout.write(msg)
-    sy.stdout.flush()
+    sys.stdout.write(msg)
+    sys.stdout.flush()
 
-# Download atmosphere files
-fname = "atm_20201217.hdf5"
-atm_file = os.path.join(
-    os.path.dirname(__file__), fname)
-new_atm_file = os.path.join(
-    os.path.dirname(__file__), "src", fname)
-url_name = ("http://pbfs.physics.berkeley.edu/BoloCalc/ATM/%s" % (fname))
-ul.urlretrieve(url_name, atm_file, reporthook)
-if os.path.exists(atm_file):
-    sy.stdout.write(
-        "\nSuccessfully downloaded atmosphere file %s." % (fname))
-    sy.stdout.write(
-        ("\nADVICE: delete any old atm files (~1 GB each) from BoloCalc" + 
-        os.sep + "src" + os.sep + "\n\n"))
-    os.rename(atm_file, new_atm_file)
-else:
-    sy.stdout.write(
-        "\nERROR: problem downloading atmosphere file %s\n\n" % (fname))
+def main():
+    # Download atmosphere files
+    fname = "atm_20201217.hdf5"
+    top_dir = os.path.abspath(os.path.dirname(__file__)).replace('/scripts', '')
+    atm_file = os.path.join(top_dir, "scripts", fname)
+    new_atm_file = os.path.join(top_dir, "config", fname)
+    print(atm_file, new_atm_file)
+    return
+
+    url_name = ("http://pbfs.physics.berkeley.edu/BoloCalc/ATM/%s" % (fname))
+    ul.urlretrieve(url_name, atm_file, reporthook)
+    if os.path.exists(atm_file):
+        sys.stdout.write("\nSuccessfully downloaded atmosphere file %s." % (fname))
+        sys.stdout.write(
+            ("\nADVICE: delete any old atm files (~1 GB each) from BoloCalc" + 
+            os.sep + "src" + os.sep + "\n\n"))
+        os.rename(atm_file, new_atm_file)
+    else:
+        sys.stdout.write("\nERROR: problem downloading atmosphere file %s\n\n" % (fname))
+
+        
+if __name__ == '__main__':
+    main()
